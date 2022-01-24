b8439866011db8197dfbebe0ba5d0528dbcba3eb
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-01-24 19:54:09 +0100

    Refacto CSS Reset and HTML head

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,7 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += "responsive";
   } else {
     x.className = "topnav";
   }
@@ -19,5 +19,3 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
