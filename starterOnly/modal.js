c8e7e9e2c4c23d9f028da6c8fe1dd5819d2611ca
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-01-30 23:23:40 +0100

    Add Regex for Validation / Validation Modal / Submit Event

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// Nav Menu Handler
 function editNav() {
   const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -9,10 +10,12 @@ function editNav() {
 
 // DOM Elements
 const modalBtn = document.querySelectorAll(".modal-btn");
+const submitBtn = document.querySelector(".btn-submit");
+const closeBtn = document.querySelector(".close");
+const form = document.forms["reserve"];
 const formData = document.querySelectorAll(".formData");
-const closeModalButton = document.querySelector(".close");
-const modalBackground = document.querySelector(".bground");
 const modal = document.querySelector(".content");
+const modalBackground = document.querySelector(".bground");
 
 // Open Modal Event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,10 +24,18 @@ function launchModal() {
 }
 
 // Close Modal Event
-closeModalButton.addEventListener("click", closeModal);
+closeBtn.addEventListener("click", closeModal);
 modalBackground.addEventListener("click", closeModal);
 function closeModal(e) {
-  if (e.target.contains(closeModalButton)) {
+  if (e.target.contains(closeBtn)) {
     modalBackground.style.display = "none";
   }
 }
+
+// Validation RegEx
+const regexName = /^[a-zA-Z0-9éè^]{2,}$/;
+const regexMail = /^[a-z0-9.-_]+@[a-z0-9.-_]{2,}\.[a-z]{2,4}$/;
+const regexQuantity = /^[0-9]$/;
+
+// Form Submission
+submitBtn.addEventListener("click", () => form.submit());
